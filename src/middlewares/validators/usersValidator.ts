aab9e570752cfde21b2ae081f1612154681f767c
長谷川 裕大 <gssbtsm999@gmail.com>
2022-02-27 10:53:34 +0900

    add examples

--- src/middlewares/validators/usersValidator.ts
+++ src/middlewares/validators/usersValidator.ts
@@ -1,3 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
 const usersValidator = {
-  //
+  create: [
+    body('name').isLength({ min: 1, max: 100 }),
+    body('age').isInt({ min: 0, max: 120 }),
+    (req: Request, res: Response, next: NextFunction) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      next();
+    },
+  ],
 };
+
+export default usersValidator;
